0c07d92b4cfd2fa9f17b419f1e56d4dea270afca
Matt McGinnis <mcginnisma90@gmail.com>
2018-10-30 09:17:16 -0400

    Helper method for user avatar seems to be working.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
-    def user_avatar_helper(user, size, style_class)
+    def user_avatar_helper(user, size, style_class, style = "none")
         if user.avatar.blank?
-            image_tag "Default-Avatar", size: size, class: style_class
+            image_tag "Default-Avatar", size: size, class: style_class, style: style
         else
-            image_tag user.avatar.url, size: size, class: style_class
+            image_tag user.avatar.url, size: size, class: style_class, style: style
         end
     end
 end
