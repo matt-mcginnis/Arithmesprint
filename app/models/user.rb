8ea57cde25917adcb60bab46a6df151ae461ac39
Matt McGinnis <mcginnisma90@gmail.com>
2018-10-30 15:18:31 -0400

    Added some more styling and validation for name in User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
     devise :database_authenticatable, :registerable,
            :recoverable, :rememberable, :validatable
 
+    validates :name, presence: true
     validates :username, presence: true, uniqueness: true
 
     serialize :friends, Array
